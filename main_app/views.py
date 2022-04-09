5e9979ed4b211e96dcb3cdc562632fd4a74b3978
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-04-09 13:46:27 -0600

    styling

--- main_app/views.py
+++ main_app/views.py
@@ -22,4 +22,4 @@ def about(request):
     return render(request, 'about.html')
 
 def slang_index(request):
-    return render(request, 'slang/index.html')
+    return render(request, 'slang/index.html', {"slangterms": slangterms})
