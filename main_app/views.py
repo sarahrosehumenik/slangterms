5a415a4f0133df9e9be71cde9925dd57c3b66565
sarahrosehumenik <sarahrosehumenik@gmail.com>
2022-04-09 02:27:33 -0600

    adding class and list

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse 
 
+class Slang:
+  def __init__(self, term, definition, example):
+      self.term = term
+      self.definition = definition
+      self.example = example
+    
+slangterms = (
+    Slang("Disney Adult", "A millenial adult,with or without kids, that can't stop talking about disney", "Raul is a Disney Adult"),
+    Slang("Wife guy", "A man who takes any opportunity to mention his wife", "raul is a wife guy"),
+    Slang("Hose Sniffer", "A person who hangs around firefighters with the intent of satisfying personal relationship desires", "raul is a hose sniffer")
+)
 
 def home(request):
     return render(request, 'home.html')
